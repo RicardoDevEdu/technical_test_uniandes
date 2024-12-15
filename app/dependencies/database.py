b931113039e81407ae0f9b1042c9d57d93d028e2
Jose A. Ricardo <jricardo73@uan.edu.co>
2024-12-15 18:54:10 -0500

    Add entry point for migration handler

---
+++ app/dependencies/database.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from sqlmodel import Session, create_engine, SQLModel
+
+SQLITE_DATABASE_URL = 'sqlite:///./inventory_starships.db'
+
+engine = create_engine(SQLITE_DATABASE_URL)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+def create_tables(app: FastAPI):
+    SQLModel.metadata.drop_all(engine)
+    SQLModel.metadata.create_all(engine, checkfirst=True)
+    yield
+
+SessionDep = Annotated[Session, Depends(get_session)]
--- app/routes/migrations.py
+++ app/routes/migrations.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlmodel import select
+
+from app.dependencies.database import SessionDep
+from app.models.database import StarshipModel, PilotModel, PilotSpecieModel, SpecieModel
+from app.services.requestor import Requestor
+from app.handlers.MigrationHandler import MigrationHandler
+from app.services.storage import Storage
 
 router = APIRouter()
 
-@router.get("/")
-def init_migrations():
-    return {"Message": "Initialization Complete"}
+@router.post("/api/migrations", response_model=dict)
+async def init_migrations(request = Depends(Requestor), store = Depends(Storage)):
+    migrations = MigrationHandler(request, store)
+    result = migrations.start()
+
+    return result
